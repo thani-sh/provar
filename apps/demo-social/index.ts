0991ea55c57150c76de7d97bea1cfece6b9940f2
Thanish Nizam <thanish.nizam@postnord.com>
2026-06-14 17:26:43 +0200

    chore: remove demo-social app from repo

--- apps/demo-social/index.ts
+++
@@ -1,9 +0,0 @@
-import { routes } from "./src/server/routes";
-
-const server = Bun.serve({
-  port: 6001,
-  routes,
-  development: true,
-});
-
-console.log(`Social Server running at ${server.url}`);
